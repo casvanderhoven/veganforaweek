efb5f79a2e1d3c4bae5804b623f899e0e8cbacce
Cas van der Hoven <casvanderhoven@cass-mbp.home>
2017-02-22 16:46:56 +0100

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,71 +1,72 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
-	before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
-	def index
-		redirect_to root_url
-	end
+  def index
+    redirect_to root_url
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			log_in @user
-			flash[:success] = "Thanks for signing up, #{@user.name}!"
-			redirect_to @user
-		else
-			render 'new'
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update_attributes(user_params)
-			flash[:success] = "Profile updated"
-			redirect_to @user
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		User.find(params[:id]).destroy
-		flash[:success] = "Your account has been deleted"
-		redirect_to root_url
-	end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Your account has been deleted"
+    redirect_to root_url
+  end
 
-	private
+  private
 
-		def user_params
-			params.require(:user).permit(:name, :email, :servings, :password, :password_confirmation)
-		end
+  def user_params
+    params.require(:user).permit(:name, :email, :servings, :password, :password_confirmation)
+  end
 
-		#Before filters
-		
-		# Confirms a logged-in user
-		def logged_in_user
-			unless logged_in?
-				store_location
-				flash[:error] = "Please log in."
-				redirect_to login_url
-			end
-		end
+  #Before filters
 
-		# Confirms the correct user
-		def correct_user
-			@user = User.find(params[:id])
-			redirect_to(root_url) unless current_user?(@user)
-		end
+  # Confirms a logged-in user
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:error] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,70 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-	before_save { email.downcase! }
-	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 },
-						format: { with: VALID_EMAIL_REGEX },
-						uniqueness: { case_sensitive: false }
-	validates :servings, presence: true, numericality: { only_integer: true }
-	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  before_save :downcase_email
+  before_create :create_activation_digest
 
-	# Returns the hash digest of the given string
-	def User.digest(string)
-		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-														BCrypt::Engine.cost
-		BCrypt::Password.create(string, cost: cost)
-	end
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  validates :servings, presence: true, numericality: { only_integer: true }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-	# Returns a random token
-	def User.new_token
-		SecureRandom.urlsafe_base64
-	end
+  # Returns the hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-	# Remembers a user in the database for use in persistent sessions
-	def remember
-		self.remember_token = User.new_token
-		update_attribute(:remember_digest, User.digest(remember_token))
-	end
+  # Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
-	# Returns true if the given token matches the digest
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
-	end
+  # Remembers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
-	# Forgets a user
-	def forget
-		update_attribute(:remember_digest, nil)
-	end
+  # Returns true if the given token matches the digest
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Activates an account
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # Sends activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  # Private functions
+  private
+
+  # Converts email to all lower-case
+  def downcase_email
+    self.email.downcase!
+  end
+
+  # Creates and assigns the activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_token = User.digest(activation_token)
+  end
+
 end
